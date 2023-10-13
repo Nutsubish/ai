f4a3348aeec2caad4f6c291edfcdba1438a7a0c5
Nutsubish <147431395+Nutsubish@users.noreply.github.com>
2023-10-13 23:50:48 +0400

    Add files via upload

---
+++ dNikoloz /1/2.py
@@ -0,0 +1,7 @@
+l = 3
+for i in range (l ,-(l+1), - 1):
+    for j in range(l , abs(i)-1, -1):
+        print("1% out of 100%",end='')
+            
+        print("skip")
+
---
+++ dNikoloz /1/3.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+screen= tk.Tk()
+
+button1=tk.Button(text = "stawt", padx= "1000",pady="1000",command= exit)
+
+button1.pack()
+ 
+screen.mainloop()
+                  
